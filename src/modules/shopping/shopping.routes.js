691bcf930f943121fd5c4d02f6b972193f878bd3
Promise Izuagbala <danwilly178@yahoo.com>
2019-07-10 18:30:43 +0100

    Update shopping.routes.js

--- src/modules/shopping/shopping.routes.js
+++ src/modules/shopping/shopping.routes.js
@@ -23,26 +23,7 @@ const uploads = multer({
 
 const routes = new Router();
 
-// var storage = multer.diskStorage({
-//   destination: function(req, file, cb) {
-//     cb(null, './ProductImage');
-//   },
-//   filename: function(req, file, cb) {
-//     cb(null, Date.now() + '-' + file.originalname);
-//   },
-// });
-// var upload = multer({ storage: storage }).single('file');
 
-// routes.post('/upload', function(req, res) {
-//   upload(req, res, function(err) {
-//     if (err instanceof multer.MulterError) {
-//       return res.status(500).json(err);
-//     } else if (err) {
-//       return res.status(500).json(err);
-//     }
-//     return res.status(200).send(req.file);
-//   });
-// });
 
 routes.post(
   '/',
